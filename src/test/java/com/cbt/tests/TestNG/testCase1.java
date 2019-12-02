a9a0b5ad48078358f481a9f8b1cea1f9affefb48
ozgenkacar <ozkcr@icloud.com>
2019-12-01 16:30:02 -0800

    second commit

--- src/test/java/com/cbt/tests/TestNG/testCase1.java
+++ src/test/java/com/cbt/tests/TestNG/testCase1.java
@@ -10,6 +10,8 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class testCase1 {
    private WebDriver driver;
 
@@ -19,6 +21,8 @@ public class testCase1 {
       driver.manage().window().maximize();
       driver.get("http://practice.cybertekschool.com/");
        UtilWait.wait(2);
+       driver.findElement(By.linkText("Registration Form")).click();
+       UtilWait.wait(1);
    }
 
    @AfterMethod
@@ -35,8 +39,6 @@ public class testCase1 {
      */
 @Test(description = "Verify that warning message is displayed:'The date of birth is not valid' ")
    public void test1(){
-   driver.findElement(By.linkText("Registration Form")).click();
-   UtilWait.wait(2);
    driver.findElement(By.cssSelector("[placeholder='MM/DD/YYYY']")).sendKeys("wrong_dob");
    UtilWait.wait(2);
    String expectedMessage = "The date of birth is not valid";
@@ -45,7 +47,41 @@ public class testCase1 {
    Assert.assertEquals(actualMesage,expectedMessage);
 
 }
+/*
+Test case #2
+Step 1. Go to “https://practice-cybertekschool.herokuapp.com”
+Step 2. Click on “Registration Form”
+Step 3. Verify that following options for programming languages are displayed: c++, java, JavaScript
+ */
+@Test(description = "Verify that following options for programming languages are displayed: c++, java, JavaScript")
+    public void test2(){
+  List<WebElement> options =driver.findElements(By.className("form-check-input"));
+    int  index =1;
+  for (WebElement each: options){
+    // Assert.assertTrue(each.isDisplayed());
 
+      if(each.isDisplayed()){
+          System.out.println(driver.findElement(By.cssSelector("[for='inlineCheckbox"+index+"']")).getText()+" programing language is displayed");
+      }else{
+          System.out.println("programing language is not displayed");
+      }
+      index++;
+  }
+}
 
-
+/*
+Test case #3 Step 1. Go to “https://practice-cybertekschool.herokuapp.com”
+Step 2. Click on “Registration Form”
+Step 3. Enter only one alphabetic character into first name input box.
+Step 4. Verify that warning message is displayed: “first name must be more than 2 and less than 64 characters long”
+ */
+@Test(description = "Verify that warning message is displayed: 'first name must be more than 2 and less than 64 characters long'")
+    public void test3(){
+    driver.findElement(By.cssSelector("[placeholder='first name']")).sendKeys("a");
+    UtilWait.wait(3);
+    String expectedMessage = "first name must be more than 2 and less than 64 characters long";
+    String actualMessage =driver.findElement(By.cssSelector("[data-bv-result='INVALID']")).getText();
+    UtilWait.wait(3);
+    Assert.assertEquals(actualMessage,expectedMessage);
+}
 }
